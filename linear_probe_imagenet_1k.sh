bc2b3c19dc7c58bcbd8297c72a39ad48ebf423ab
Kevin_Z17 <96039456+KevinZ0217@users.noreply.github.com>
2025-07-04 11:16:13 -0400

    Create linear_probe_imagenet_1k.sh

---
+++ linear_probe_imagenet_1k.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+STEP=$1
+OUTPUT=$2
+
+python dinov2/run/eval/linear.py \
+	--config-file $OUTPUT/config.yaml \
+        --pretrained-weights $OUTPUT/eval/training_$STEP/teacher_checkpoint.pth \
+        --output-dir $OUTPUT/eval/training_$STEP/linear \
+	--train-dataset ImageNet:split=TRAIN:root=imagenet:extra=imagenet-extra \
+        --val-dataset ImageNet:split=VAL:root=imagenet/:extra=imagenet-extra
