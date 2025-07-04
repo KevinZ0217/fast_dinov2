7d3dbdfd1fe2202990b27aaf5a4eeb52ff42dad4
Kevin_Z17 <96039456+KevinZ0217@users.noreply.github.com>
2025-07-04 11:15:44 -0400

    Update linear_probe_imagenet_100.sh

--- linear_probe_imagenet_100.sh
+++ linear_probe_imagenet_100.sh
@@ -6,5 +6,5 @@ python dinov2/run/eval/linear.py \
 	--config-file $OUTPUT/config.yaml \
         --pretrained-weights $OUTPUT/eval/training_$STEP/teacher_checkpoint.pth \
         --output-dir $OUTPUT/eval/training_$STEP/linear \
-	--train-dataset ImageNet:split=TRAIN:root=ILSVRC2012:extra=imagenet-extra \
-        --val-dataset ImageNet:split=VAL:root=imagenet/:extra=imagenet-extra
+	--train-dataset ImageNet:split=TRAIN:root=mini-imagenet:extra=-mini-imagenet-extra \
+        --val-dataset ImageNet:split=VAL:root=mini-imagenet/:extra=mini-imagenet-extra
