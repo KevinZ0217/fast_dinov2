5099959f2974ca9ba1afa877fbdf441afe2ab00d
Kevin_Z17 <96039456+KevinZ0217@users.noreply.github.com>
2025-07-04 11:10:25 -0400

    Update and rename linear_probe.sh to linear_probe_imagenet_100.sh

--- linear_probe_imagenet_100.sh
+++ linear_probe_imagenet_100.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-
-#SBATCH -n 2
-#SBATCH -c 6
-#SBATCH --mem=192G
-#SBATCH --gres=gpu:a5000:1
-#SBATCH --nodes=1
-#SBATCH -t 2:00:00
-#SBATCH -p gpu
-
 STEP=$1
 OUTPUT=$2
 
-PYTHONPATH="robust_dinov2" python dinov2/run/eval/linear.py \
+python dinov2/run/eval/linear.py \
 	--config-file $OUTPUT/config.yaml \
         --pretrained-weights $OUTPUT/eval/training_$STEP/teacher_checkpoint.pth \
         --output-dir $OUTPUT/eval/training_$STEP/linear \
