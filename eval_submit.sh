6270226157c4968c4cb80f88da9347b2064407d1
Kevin_Z17 <96039456+KevinZ0217@users.noreply.github.com>
2025-07-09 14:03:14 -0700

    Update eval_submit.sh

--- eval_submit.sh
+++ eval_submit.sh
@@ -2,26 +2,21 @@
 
 last_job_id=""
 corruptions=(
-    #elastic_transform frost shot_noise speckle_noise
-    #contrast impulse_noise pixelate snow fog
-    #gaussian_noise jpeg_compression saturate spatter
-    #brightness
-brightness elastic_transform frost glass_blur 
-motion_blur shot_noise speckle_noise contrast
-gaussian_blur impulse_noise pixelate snow zoom_blur 
-defocus_blur fog gaussian_noise jpeg_compression saturate spatter
-
+	brightness elastic_transform frost glass_blur 
+	motion_blur shot_noise speckle_noise contrast
+	gaussian_blur impulse_noise pixelate snow zoom_blur 
+	defocus_blur fog gaussian_noise jpeg_compression saturate spatter
 )
 levels=(1 2 3 4 5) 
-steps=(124999)
-output=(resume_150_1K_gp)
+steps=([the checkpoint step to evaluate(i.e, 12499)])
+output=([your_output_folder])
 for step in "${steps[@]}"; do
 	for corruption in "${corruptions[@]}"; do
 	    for level in "${levels[@]}"; do
 	    	if [ -z "$last_job_id" ]; then
-            		job_id=$(sbatch --parsable --job-name=${corruption}_${level}_${step} eval_main.sh $corruption $level $step $output)
+            		job_id=$(sh --parsable --job-name=${corruption}_${level}_${step} eval_main.sh $corruption $level $step $output)
             	else
-            		job_id=$(sbatch --parsable --dependency=afterok:$last_job_id --job-name=${corruption}_${level}_${step} eval_main.sh $corruption $level $step $output)
+            		job_id=$(sh --parsable --dependency=afterok:$last_job_id --job-name=${corruption}_${level}_${step} eval_main.sh $corruption $level $step $output)
         	fi
         
         	last_job_id=$job_id
