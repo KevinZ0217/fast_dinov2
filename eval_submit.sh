9a0688b598b35695c7f9b551173d85b5db59791a
Kevin_Z17 <96039456+KevinZ0217@users.noreply.github.com>
2025-07-09 14:08:17 -0700

    Update eval_submit.sh

--- eval_submit.sh
+++ eval_submit.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-last_job_id=""
 corruptions=(
 	brightness elastic_transform frost glass_blur 
 	motion_blur shot_noise speckle_noise contrast
@@ -8,19 +7,14 @@ corruptions=(
 	defocus_blur fog gaussian_noise jpeg_compression saturate spatter
 )
 levels=(1 2 3 4 5) 
-steps=([the checkpoint step to evaluate(i.e, 12499)])
-output=([your_output_folder])
+steps=(12499)  # put your checkpoint steps here
+output="your_output_folder"
+
 for step in "${steps[@]}"; do
 	for corruption in "${corruptions[@]}"; do
 	    for level in "${levels[@]}"; do
-	    	if [ -z "$last_job_id" ]; then
-            		job_id=$(sh --parsable --job-name=${corruption}_${level}_${step} eval_main.sh $corruption $level $step $output)
-            	else
-            		job_id=$(sh --parsable --dependency=afterok:$last_job_id --job-name=${corruption}_${level}_${step} eval_main.sh $corruption $level $step $output)
-        	fi
-        
-        	last_job_id=$job_id
-        	echo "Submitted job $job_id (${corruption}-${level}) depending on $last_job_id"
+	        echo "Running eval_main.sh $corruption $level $step $output"
+	        sh eval_main.sh $corruption $level $step $output
 	    done
 	done
 done
