b045d3a29386a1f0255a25d409e129590fae2a4f
Kevin_Z17 <96039456+KevinZ0217@users.noreply.github.com>
2025-07-09 16:25:47 -0700

    Update linear_probe_submit_100.sh

--- linear_probe_submit_100.sh
+++ linear_probe_submit_100.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-last_job_id=""
 steps=(
 249999 237499 224999 212499 199999 187499 174999 162499 149999 137499 124999 112499 99999 87499 74999 62499 49999 37499 24999 12499
 )
-output=(restart_100)
-for step in "${steps[@]}"; do
-                if [ -z "$last_job_id" ]; then
-                        job_id=$(sh linear_probe_imagenet_100.sh $step $output)
-                else
-                        job_id=$(sh linear_probe_imagenet_100.sh $step $output)
-                fi
+output="<output folder name from pretraining>"
 
-                last_job_id=$job_id
-                echo "Submitted job $job_id (${step}) depending on $last_job_id"
+for step in "${steps[@]}"; do
+    echo "Running linear_probe_imagenet_100.sh with step $step and output $output"
+    sh linear_probe_imagenet_100.sh "$step" "$output"
 done
