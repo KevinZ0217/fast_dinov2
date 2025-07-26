51af144c9221f21533ed01dd7fe33de2a5f8b8a4
Kevin_Z17 <96039456+KevinZ0217@users.noreply.github.com>
2025-07-26 04:55:36 -0400

    Update train_imagenet_100.sh

--- train_imagenet_100.sh
+++ train_imagenet_100.sh
@@ -1,8 +1,8 @@
 #low resolution training phase for 150 epochs
 JOB_ID=$(python dinov2/run/train/train.py \
     --nodes 1 \
-    --config-file dinov2/configs/ssl_200_size112_config.yaml \
-    --output-dir ssl_200_size112 \
+    --config-file dinov2/configs/ssl_200_size112_100_config.yaml \
+    --output-dir ssl_200_size112_100_output \
     train.dataset_path=ImageNet:split=TRAIN:root=mini-imagenet:extra=mini-imagenet-extra)
 
 #restart the training using the checkpoint at the 150th epochs
